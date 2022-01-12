58eb36bd64336772d81d746e0cf3b0047ff86d39
Samuel Obe <samuel.obe@outlook.com>
2022-01-12 11:43:25 -0500

    Created a basic search bar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import "./SearchBar.css";
+
+const SearchBar = props => {
+    return (
+        <div>
+            <input 
+                type="text"
+                placeholder='Search for city'
+                className='search-bar'
+             />
+        </div>
+    )
+}
+
+export default SearchBar
